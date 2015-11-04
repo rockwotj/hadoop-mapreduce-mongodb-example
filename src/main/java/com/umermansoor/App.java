5aabaf695303f8bfb5724afc5c2360240cc1ec65
Tyler Rockwood <rockwotj@users.noreply.github.com>
2015-11-04 16:01:16 -0500

    Updating starting code

--- src/main/java/com/umermansoor/App.java
+++ src/main/java/com/umermansoor/App.java
@@ -1,34 +1,39 @@
 package com.umermansoor;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 /**
  * The main application class. 
  * 
  * @author Umer Mansoor
  */
-public class App 
+public class App extends Configured implements Tool
 {
     /**
      * Application entry point.
      * @param args
      * @throws Exception - Bad idea but produces less cluttered code.
      */
-    public static void main(String[] args) throws Exception {
+    public int run(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: hadoopex <input path> <output path>");
-            System.exit(-1);
+            System.err.println("Usage: <input path> <output path>");
+            return 1;
         }
 
         // Create the job specification object
-        Job job = new Job();
+        Configuration conf = getConf();
+        Job job = Job.getInstance(conf);
         job.setJarByClass(App.class);
-        job.setJobName("Earthquake Measurment");
+        job.setJobName("Earthquake Measurement");
 
         // Setup input and output paths
         FileInputFormat.addInputPath(job, new Path(args[0]));
@@ -43,6 +48,11 @@ public class App
         job.setOutputValueClass(DoubleWritable.class);
 
         // Wait for the job to finish before terminating
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
+    
+    public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new App(), args);
+		System.exit(res);
+	}
 }
